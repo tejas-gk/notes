d544ab689bf438f822afcb0bcd3e0e8da22fcf57
tejas-gk <tejgankri@outlook.com>
2023-01-05 06:17:45 +0530

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,82 @@
+
+import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main'
+import { useEffect, useState } from 'react'
+import uuid from 'react-uuid'
+import styles from './App.css'
+interface Note {
+  id: string;
+  title: string;
+  body: string;
+  lastModified: number;
+}
+
+export default function Home() {
+  // @ts-ignore
+  const [notes, setNotes] = useState(
+    localStorage.notes ? JSON.parse(localStorage.notes) : []
+  )
+  const [activeNote, setActiveNote] = useState<boolean|any>(false)
+    const onAddNote = () => {
+        const newNote:Note = {
+      id: uuid(),
+      title: "Untitled Note",
+      body: "",
+      lastModified: Date.now(),
+    };
+
+    setNotes([newNote, ...notes]);
+    setActiveNote(newNote.id);
+    }
+  
+  const onDeleteNote = (id: string) => {
+    setNotes(notes.filter((note) => note.id !== id))
+    console.log(id)
+  }
+
+  const getActiveNote = () => {
+    // @ts-ignore
+    return notes.find(({ id }) => id === activeNote);
+  }
+
+  const onUpdateNote = (updatedNote: Note) => {
+    const updatedNotesArray = notes.map((note) => {
+      // @ts-ignore
+      if (note.id === updatedNote.id) {
+        return updatedNote
+      }
+      return note
+    })
+    setNotes(updatedNotesArray)
+  }
+
+   useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+
+  return (
+    <>
+
+
+      <div className='flex flex-row'>
+      <section>
+          <Sidebar
+            notes={notes}
+            onAddNote={onAddNote}
+            onDeleteNote={onDeleteNote}
+            activeNote={activeNote}
+            setActiveNote={setActiveNote}
+          />
+      </section>
+      <main className={styles.main}>
+          <Main
+            activeNote={getActiveNote()}
+            onUpdateNote={onUpdateNote}
+          />
+        </main>
+      </div>
+      
+    </>
+  )
+}
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+interface SidebarProps {
+    notes: any;
+    onAddNote: () => void;
+    onDeleteNote: (id: number | any) => void;
+    activeNote: boolean;
+    setActiveNote: (activeNote: boolean) => void;
+}
+
+export default function Sidebar({
+    notes,
+    onAddNote,
+    onDeleteNote,
+    activeNote,
+    setActiveNote
+}: SidebarProps) {
+   const sortedNotes = notes.sort((a:any, b:any) => b.lastModified - a.lastModified);
+
+  return (
+      <div>
+          <div className="flex flex-col  w-64 h-screen bg-[#2c2c38]">
+              <div className="sidebar-header flex flex-row items-center justify-between w-full h-20 bg-gray-900">
+                  <h1 className="text-2xl font-bold text-white pl-5">Notes</h1>
+                  <div className='text-white pr-10'>
+                      <button
+                          onClick={onAddNote}
+                      >Add</button>
+                  </div>
+              </div>
+              <div className='app-sidebar-notes h-[calc(100vh-78vh)]' >
+              {
+                  sortedNotes?.map(({ id, title, body, lastModified }:any,key:number) => (
+                      <div className={`sidebar-notes cursor-pointer p-4  hover:bg-black
+                            ${
+                          // @ts-ignore
+                          id === activeNote && 'bg-black'}
+                      `}
+                          onClick={()=>{setActiveNote(id)}}
+                          key={key}>
+                      <div className='sidebar-note-title flex justify-between items-center'>
+                              <h1 className='text-white font-semibold'>{title}</h1>
+                              <button className='text-red-500 hover:text-red-600'
+                                  onClick={()=>{onDeleteNote(id)}}
+                              >delete</button>
+                      </div>
+                      <div className='note-preview flex flex-col'>
+                              <small className='text-gray-400'>{
+                                    body.length > 10 ? body.substr(0,10) + '...' : body
+                          }</small>
+                              <small className='text-gray-400 inline-block'>{
+                                  new Date(lastModified).toLocaleDateString('en-US', {
+                                      hour: 'numeric',
+                                      minute: 'numeric'
+                                  })
+                                  
+                              }</small>
+                      </div>
+                  </div>
+                    ))
+                    
+                }
+                </div>
+          </div>
+          
+      </div>
+  )
+}
