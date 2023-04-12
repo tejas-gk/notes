134402409f62363eec3d923acfe3e7e0f170eeae
tejas-gk <tejgankri@outlook.com>
2023-04-12 20:13:16 +0530

    chore:removed un used code

--- src/App.tsx
+++ src/App.tsx
@@ -1,103 +1,100 @@
-
-import Sidebar from './components/Sidebar/Sidebar'
-import Main from './components/Main/Main'
-import { useEffect, useLayoutEffect, useState } from 'react'
-import uuid from 'react-uuid'
-import styles from './App.css'
+import Sidebar from './components/Sidebar/Sidebar';
+import Main from './components/Main/Main';
+import { useEffect, useLayoutEffect, useState } from 'react';
+import uuid from 'react-uuid';
+import styles from './App.css';
 
 interface Note {
-  id: string;
-  title: string;
-  body: string;
-  lastModified: number;
+    id: string;
+    title: string;
+    body: string;
+    lastModified: number;
 }
 
 export default function Home() {
-  // @ts-ignore
-  const [notes, setNotes] = useState(
-    localStorage.notes ? JSON.parse(localStorage.notes) : []
-  )
-  const [activeNote, setActiveNote] = useState<boolean|any>(false)
+    // @ts-ignore
+    const [notes, setNotes] = useState(
+        localStorage.notes ? JSON.parse(localStorage.notes) : []
+    );
+    const [activeNote, setActiveNote] = useState<boolean | any>(false);
     const onAddNote = () => {
-        const newNote:Note = {
-      id: uuid(),
-      title: "Untitled Note",
-      body: "",
-      lastModified: Date.now(),
+        const newNote: Note = {
+            id: uuid(),
+            title: 'Untitled Note',
+            body: '',
+            lastModified: Date.now(),
+        };
+
+        setNotes([newNote, ...notes]);
+        setActiveNote(newNote.id);
     };
 
-    setNotes([newNote, ...notes]);
-    setActiveNote(newNote.id);
-    }
-  
-  const onDeleteNote = (id: string) => {
-    // @ts-ignore
-    setNotes(notes.filter((note) => note.id !== id))
-    console.log(id)
-  }
+    const onDeleteNote = (id: string) => {
+        // @ts-ignore
+        setNotes(notes.filter((note) => note.id !== id));
+        console.log(id);
+    };
 
-  const getActiveNote = () => {
-    // @ts-ignore
-    return notes.find(({ id }) => id === activeNote);
-  }
+    const getActiveNote = () => {
+        // @ts-ignore
+        return notes.find(({ id }) => id === activeNote);
+    };
 
-  const onUpdateNote = (updatedNote: Note) => {
-    // @ts-ignore
-    const updatedNotesArray = notes.map((note) => {
-      // @ts-ignore
-      if (note.id === updatedNote.id) {
-        return updatedNote
-      }
-      return note
-    })
-    setNotes(updatedNotesArray)
-  }
+    const onUpdateNote = (updatedNote: Note) => {
+        // @ts-ignore
+        const updatedNotesArray = notes.map((note) => {
+            // @ts-ignore
+            if (note.id === updatedNote.id) {
+                return updatedNote;
+            }
+            return note;
+        });
+        setNotes(updatedNotesArray);
+    };
 
-   useEffect(() => {
-    localStorage.setItem("notes", JSON.stringify(notes));
-  }, [notes]);
+    useEffect(() => {
+        localStorage.setItem('notes', JSON.stringify(notes));
+    }, [notes]);
 
-  const isMobile = () => {
-    return window.innerWidth <= 800;
-  };
+    const isMobile = () => {
+        return window.innerWidth <= 800;
+    };
 
-  if (isMobile()) {
-    return (
-      <div className="
+    if (isMobile()) {
+        return (
+            <div
+                className='
       flex flex-col items-center justify-center h-screen
-      ">
-        <p className="text-2xl font-bold">Note App</p>
-        <p className="text-sm px-3">
+      '
+            >
+                <p className='text-2xl font-bold'>Note App</p>
+                <p className='text-sm px-3'>
+                    This app works best on a desktop or laptop. Please open it
+                    on a larger screen.
+                </p>
+            </div>
+        );
+    }
 
-          This app works best on a desktop or laptop. Please open it on a larger screen.
-        </p>
-      </div>
+    return (
+        <>
+            <div className='flex flex-row'>
+                <section>
+                    <Sidebar
+                        notes={notes}
+                        onAddNote={onAddNote}
+                        onDeleteNote={onDeleteNote}
+                        activeNote={activeNote}
+                        setActiveNote={setActiveNote}
+                    />
+                </section>
+                <main>
+                    <Main
+                        activeNote={getActiveNote()}
+                        onUpdateNote={onUpdateNote}
+                    />
+                </main>
+            </div>
+        </>
     );
-  }
-
-
-  return (
-    <>
-
-
-      <div className='flex flex-row'>
-      <section>
-          <Sidebar
-            notes={notes}
-            onAddNote={onAddNote}
-            onDeleteNote={onDeleteNote}
-            activeNote={activeNote}
-            setActiveNote={setActiveNote}
-          />
-      </section>
-      <main>
-          <Main
-            activeNote={getActiveNote()}
-            onUpdateNote={onUpdateNote}
-          />
-        </main>
-      </div>
-      
-    </>
-  )
 }
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,48 +1,58 @@
-import React from 'react'
+import React from 'react';
 interface Note {
-  id: string;
-  title: string;
-  body: string;
-  lastModified: number;
+    id: string;
+    title: string;
+    body: string;
+    lastModified: number;
 }
 
 interface MainProps {
-  activeNote: any;
-  onUpdateNote: (updatedNote: any) => Note | void;
+    activeNote: any;
+    onUpdateNote: (updatedNote: any) => Note | void;
 }
 
-export default function Main({
-  activeNote,
-  onUpdateNote,
-}: MainProps) {
-  
-  const onEditField = (key: string, value: string) => {
-    onUpdateNote({
-       ...activeNote,
-      [key]: value,
-      lastModified: Date.now(),
-    })
-  }
+export default function Main({ activeNote, onUpdateNote }: MainProps) {
+    const onEditField = (key: string, value: string) => {
+        onUpdateNote({
+            ...activeNote,
+            [key]: value,
+            lastModified: Date.now(),
+        });
+    };
 
-  if (!activeNote) return <div className='no-active-note text-3xl text-gray-400 font-bold text-center mt-56 ml-96
-  '>No note selected</div>
+    if (!activeNote)
+        return (
+            <div
+                className='no-active-note text-3xl text-gray-400 font-bold text-center mt-56 ml-96
+  '
+            >
+                No note selected
+            </div>
+        );
 
-  return (
-      <div className='app-main w-full h-full ml-10 mt-10'> 
-          <div className='app-main-note-edit'>
-              <input type="text" className='w-full h-auto 
-               text-3xl font-bold border-b-2 outline-none' placeholder='Note Title'
-          value={activeNote?.title}
-          onChange={(e)=>{onEditField('title', e.target.value)}}
-          autoFocus />
-        <textarea className='w-full h-auto outline-none'
-          placeholder='Note Content'
-          value={activeNote?.body}
-          onChange={(e)=>{onEditField('body', e.target.value)}}
-        />
-          </div>
-     
-
-    </div>
-  )
+    return (
+        <div className='app-main w-full h-full ml-10 mt-10'>
+            <div className='app-main-note-edit'>
+                <input
+                    type='text'
+                    className='w-full h-auto 
+               text-3xl font-bold border-b-2 outline-none'
+                    placeholder='Note Title'
+                    value={activeNote?.title}
+                    onChange={(e) => {
+                        onEditField('title', e.target.value);
+                    }}
+                    autoFocus
+                />
+                <textarea
+                    className='w-full h-auto outline-none'
+                    placeholder='Note Content'
+                    value={activeNote?.body}
+                    onChange={(e) => {
+                        onEditField('body', e.target.value);
+                    }}
+                />
+            </div>
+        </div>
+    );
 }
